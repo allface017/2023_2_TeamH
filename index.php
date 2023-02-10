f02195e9a0f32b5bcfee5e956ad03b88079a57c7
miyanokengo <113155752+miyanokengo@users.noreply.github.com>
2023-02-10 12:35:52 +0900

    担当のTOP・管理者メニュー・ログインページの作成（未完成）
    一回目

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        .title {
+            text-align:center;
+            font-size: 200%;
+        }
+        .button_wrapper1 {
+            display: flex;
+            flex-direction: column;
+            margin: 100px 30% 0 30%;
+        }
+        .button_wrapper2 {
+            display: flex;
+            flex-direction: column;
+            margin: 100px 30% 0 30%;
+        }
+
+    </style>
+</head>
+<body>
+    <div class="title">
+        <h2>スポーツtopics</h2>
+
+    <div class="button_wrapper1">
+        <button onclick="location.href='./Login.php'">ログイン・新規登録</button>
+    </div>
+
+    <div class="button_wrapper2">
+        <button onclick="location.href='./#'">記事の一覧</button>
+    </div>
+    </div>
+    </body>
+</html>
