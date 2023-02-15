d4469123e3c6e8bf2cd44f2101f02fe7758a0435
miyanokengo <113155752+miyanokengo@users.noreply.github.com>
2023-02-15 11:14:22 +0900

    ログインとtop
    ログインは未完成

--- Login.php
+++ Login.php
@@ -1,1 +1,79 @@
-{\rtf1}
+<?php
+require_once 'db_connect.php';
+echo $_POST['user_name'];
+echo $_POST['password'];
+
+//エラーの処理
+//ユーザー名とパスワードが入力されているかチェック
+if(!empty($_POST['user_name']) && !empty($_POST['password'])){
+    echo '成功';
+
+//ユーザー名を使ってデータベースにユーザーが存在するかチェックする
+    $sql="select * from admin where name = :name";
+
+    $stm=$pdo->prepare($sql);
+
+    $stm->bindValue(':name', $_POST['user_name'], PDO::PARAM_STR);
+
+    $stm->execute();
+
+    $result=$stm->fetch(PDO::FETCH_ASSOC);
+    echo"<pre>";
+    var_dump($result);
+    echo"<pre>";
+
+//データベースにあるパスワードと入力されたパスワードが一致するかチェックする
+    if(!empty($result)){
+        echo 'いるよ';
+        //$resultの中にあるパスワードと入力されたパスワードが一致するかどうか
+        if($result['password'] === $_POST['password']){
+            echo 'パスワード一致';
+            header("Location:Admin.php");
+        //ログインが成功したらadmin.phpにリダイレクトする
+        }
+
+    }
+}else{
+    echo '未入力の項目があります。';
+}
+
+
+?>
+<!DOCTYPE HTML>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>ログイン</title>
+        <style type="text/css">
+            .error {
+                color: red;
+            }
+        </style>
+    </head>
+    <body>
+        <div id="wrapper">
+            <form action="Login.php" method="post">
+                <?php if (isset($err['login'])) : ?>
+                    <p class="error"><?php echo h($err['login']); ?></p>
+                <?php endif; ?>
+                <p>
+                    <label for="user_name">ユーザー名</label>
+                    <input id="user_id" name="user_name" type="text" />
+                    <?php if (isset($err['user_name'])) : ?>
+                        <p class="error"><?php echo h($err['user_name']); ?></p>
+                    <?php endif; ?>
+                </p>
+                <p>
+                    <label for="">パスワード</label>
+                    <input id="password" name="password" type="password" />
+                    <?php if (isset($err['password'])) : ?>
+                        <p class="error"><?php echo h($err['password']); ?></p>
+                    <?php endif; ?>
+                </p>
+                <p>
+                    <button type="submit">ログイン</button>
+                </p>
+            </form>
+        </div>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -6,35 +6,138 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>スポーツtopics</title>
     <style>
+        .back{
+            background-color: #00aaff;
+            border-radius: 40px 40px 40px 40px;
+            width: 80%;
+            margin-left: 10%;
+            margin-top: 40px;
+            padding-top: 10px;
+            padding-bottom:100px;
+        }
         .title {
             text-align:center;
             font-size: 200%;
         }
-        .button_wrapper1 {
-            display: flex;
-            flex-direction: column;
-            margin: 100px 30% 0 30%;
+        .btn01{
+            /*影の基点とするためrelativeを指定*/
+            position: relative;
+             /*ボタンの形状*/
+            text-decoration: none;
+            display: inline-block;
+            text-align: center;
+            background: transparent;
+            border-radius: 25px;
+            border: solid 1px #333;
+            outline: none;
+           /*アニメーションの指定*/
+            transition: all 0.2s ease;
         }
-        .button_wrapper2 {
-            display: flex;
-            flex-direction: column;
-            margin: 100px 30% 0 30%;
+        .btn01:hover{
+         border-color:transparent; 
         }
-
+        /*ボタンの中のテキスト*/
+        .btn01 span {
+            position: relative;
+             z-index: 2;/*z-indexの数値をあげて文字を背景よりも手前に表示*/
+            /*テキストの形状*/
+            display: block;
+            padding: 10px 30px;
+            background:#fff;
+            border-radius: 25px;
+            color:#333;
+            /*アニメーションの指定*/
+            transition: all 0.3s ease;
+        }
+        /*影の設定*/
+        .pushright:before {
+            content: "";
+            /*絶対配置で影の位置を決める*/
+            position: absolute;
+            z-index: -1;
+            top: 4px;
+            left: 4px;
+            /*影の形状*/
+            width: 100%;
+            height: 100%;
+            border-radius: 25px;
+            background-color: #333;
+        }
+        /*hoverの際にX・Y軸に4pxずらす*/
+        .pushright:hover span {
+            background-color: #333;
+            color: #fff;
+            transform: translate(4px, 4px);
+        }
+        .btn02{
+            /*影の基点とするためrelativeを指定*/
+            position: relative;
+             /*ボタンの形状*/
+            text-decoration: none;
+            display: inline-block;
+            text-align: center;
+            background: transparent;
+            border-radius: 25px;
+            border: solid 1px #333;
+            outline: none;
+           /*アニメーションの指定*/
+            transition: all 0.2s ease;
+        }
+        .btn02:hover{
+         border-color:transparent; 
+        }
+        /*ボタンの中のテキスト*/
+        .btn02 span {
+            position: relative;
+             z-index: 2;/*z-indexの数値をあげて文字を背景よりも手前に表示*/
+            /*テキストの形状*/
+            display: block;
+            padding: 10px 30px;
+            background:#fff;
+            border-radius: 25px;
+            color:#333;
+            /*アニメーションの指定*/
+            transition: all 0.3s ease;
+        }
+        /*影の設定*/
+        .pushright:before {
+            content: "";
+            /*絶対配置で影の位置を決める*/
+            position: absolute;
+            z-index: -1;
+            top: 4px;
+            left: 4px;
+            /*影の形状*/
+            width: 100%;
+            height: 100%;
+            border-radius: 25px;
+            background-color: #333;
+        }
+        /*hoverの際にX・Y軸に4pxずらす*/
+        .pushright:hover span {
+            background-color: #333;
+            color: #fff;
+            transform: translate(4px, 4px);
+        }
+        .button_wrapper2{
+            margin-top: 10%;
+        }
     </style>
 </head>
 <body>
+    <div class="back">
     <div class="title">
         <h2>スポーツtopics</h2>
 
     <div class="button_wrapper1">
-        <button onclick="location.href='./Login.php'">ログイン・新規登録</button>
+        <a href="Login.php" class="btn01 pushright"><span>ログイン・新規登録</span></a>
     </div>
 
     <div class="button_wrapper2">
-        <button onclick="location.href='./#'">記事の一覧</button>
+        <a href="#" class="btn02 pushright"><span>記事の一覧</span></a>
+    </div>
     </div>
     </div>
     </body>
