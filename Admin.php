7dfcac62a6dd0aba4be6b32feebe6a687f3327af
miyanokengo <113155752+miyanokengo@users.noreply.github.com>
2023-02-16 12:23:34 +0900

    管理者メニュー訂正
    訂正

--- Admin.php
+++ Admin.php
@@ -1,190 +1,152 @@
 <?php
-require_once 'db_connect.php';
-//echo $_POST['user_name'];
-//echo $_POST['password'];
-
-//エラーの処理
-//ユーザー名とパスワードが入力されているかチェック
-if(isset($_POST['user_name']) || isset($_POST['password'])){
-
-    if(!empty($_POST['user_name']) && !empty($_POST['password'])){
-        
-        //ユーザー名を使ってデータベースにユーザーが存在するかチェックする
-        $sql="select * from admin where name = :name";
-        
-        $stm=$pdo->prepare($sql);
-        
-        $stm->bindValue(':name', $_POST['user_name'], PDO::PARAM_STR);
-        
-        $stm->execute();
-        
-        $result=$stm->fetch(PDO::FETCH_ASSOC);
-        //echo"<pre>";
-        //var_dump($result);
-        //echo"<pre>";
-
-    //データベースにあるパスワードと入力されたパスワードが一致するかチェックする
-    if(!empty($result)){
-        
-        //$resultの中にあるパスワードと入力されたパスワードが一致するかどうか
-        if($result['password'] === $_POST['password']){
-            // echo 'パスワード一致';
-            header("Location:Admin.php");
-            //ログインが成功したらadmin.phpにリダイレクトする
-        }else{
-             echo '<a style="color:#ff0000;font-size: 20px;">ユーザー名またはパスワードが違います。</a><br>';
-            // echo '<p">ユーザー名またはパスワードが違います。</p>';
-        }
-    }
-}else{
-    echo '<a style="color:#ff0000;font-size: 20px;">　　　　　　未入力の項目があります。</a><br>';
-    // echo '<a style="color:#ff0000;font-size: 12px;">未入力の項目があります。</a>';
-}
-}
 ?>
-<!DOCTYPE HTML>
-<html lang="ja">
-    <head>
-        <meta charset="UTF-8">
-        <title>ログイン</title>
-        <style>
-        
-           a{
-      font-size: 50px;
-    color: white;
-    text-decoration: none;
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>管理者メニュー</title>
+    <style>
+        h2{
+            color: white;
+        }
+          .back{
+            background: linear-gradient(90deg, rgb(22, 135, 237), rgb(20, 55, 90));
+            border-radius: 40px 40px 40px 40px;
+            width: 80%;
+            margin-left: 10%;
+            margin-top: 40px;
+            padding-top: 10px;
+            padding-bottom:100px;
+        }
+        .title {
+            text-align:center;
+            font-size: 200%;
+        }
+        .btn01{
+            /*影の基点とするためrelativeを指定*/
+            position: relative;
+             /*ボタンの形状*/
+            text-decoration: none;
+            display: inline-block;
+            text-align: center;
+            background: transparent;
+            border-radius: 25px;
+            border: solid 1px #333;
+            outline: none;
+           /*アニメーションの指定*/
+            transition: all 0.2s ease;
+        }
+        .btn01:hover{
+             border-color:transparent; 
+        }
+        /*ボタンの中のテキスト*/
+        .btn01 span {
+            position: relative;
+             z-index: 2;/*z-indexの数値をあげて文字を背景よりも手前に表示*/
+            /*テキストの形状*/
+            display: block;
+            padding: 10px 30px;
+            background:#fff;
+            border-radius: 25px;
+            color:#333;
+            /*アニメーションの指定*/
+            transition: all 0.3s ease;
+        }
+        /*影の設定*/
+        .pushright:before {
+            content: "";
+            /*絶対配置で影の位置を決める*/
+            position: absolute;
+            z-index: -1;
+            top: 4px;
+            left: 4px;
+            /*影の形状*/
+            width: 100%;
+            height: 100%;
+            border-radius: 25px;
+            background-color: #333;
+        }
+        /*hoverの際にX・Y軸に4pxずらす*/
+        .pushright:hover span {
+            background-color: #333;
+            color: #fff;
+            transform: translate(4px, 4px);
+        }
+        .btn02{
+            /*影の基点とするためrelativeを指定*/
+            position: relative;
+             /*ボタンの形状*/
+            text-decoration: none;
+            display: inline-block;
+            text-align: center;
+            background: transparent;
+            border-radius: 25px;
+            border: solid 1px #333;
+            outline: none;
+           /*アニメーションの指定*/
+            transition: all 0.2s ease;
+        }
+        .btn02:hover{
+           border-color:transparent; 
+        }
+        /*ボタンの中のテキスト*/
+        .btn02 span {
+            position: relative;
+             z-index: 2;/*z-indexの数値をあげて文字を背景よりも手前に表示*/
+            /*テキストの形状*/
+            display: block;
+            padding: 10px 30px;
+            background:#fff;
+            border-radius: 25px;
+            color:#333;
+            /*アニメーションの指定*/
+            transition: all 0.3s ease;
+        }
+        /*影の設定*/
+        .pushright:before {
+            content: "";
+            /*絶対配置で影の位置を決める*/
+            position: absolute;
+            z-index: -1;
+            top: 4px;
+            left: 4px;
+            /*影の形状*/
+            width: 100%;
+            height: 100%;
+            border-radius: 25px;
+            background-color: #333;
+        }
+        /*hoverの際にX・Y軸に4pxずらす*/
+        .pushright:hover span {
+            background-color: #333;
+            color: #fff;
+            transform: translate(4px, 4px);
+        }
+        .button_wrapper2{
+            margin-top: 10%;
+        }
+    </style>
+</head>
+<body>
+<div class="back">
+    <div class="title">
+        <h2>管理者メニュー</h2>
 
-  }
-    @import url(https://fonts.googleapis.com/css?family=Roboto:300);
+    <div class="button_wrapper1">
+        <a href="Admin-List.php" class="btn01 pushright"><span>記事の一覧</span></a>
+    </div>
 
-.login-page {
-  width: 360px;
-  padding: 3% 0 0;
-  margin: auto;
-}
-.form {
-  position: relative;
-  z-index: 1;
-  background: #FFFFFF;
-  max-width: 360px;
-  margin: 0 auto 100px;
-  padding: 45px;
-  text-align: center;
-  box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
-}
-.form a{
-    color:red;
-    font-size: 15px;
-    padding: 5px;
-}
-.form input {
-  font-family: "Roboto", sans-serif;
-  outline: 0;
-  background: #f2f2f2;
-  width: 100%;
-  border: 0;
-  margin: 0 0 15px;
-  padding: 15px;
-  box-sizing: border-box;
-  font-size: 14px;
-}
-.form button {
-  font-family: "Roboto", sans-serif;
-  text-transform: uppercase;
-  outline: 0;
-  background: #4CAF50;
-  width: 100%;
-  border: 0;
-  padding: 15px;
-  color: #FFFFFF;
-  font-size: 14px;
-  -webkit-transition: all 0.3 ease;
-  transition: all 0.3 ease;
-  cursor: pointer;
-}
-.form button:hover,.form button:active,.form button:focus {
-  background: #43A047;
-}
-.form .message {
-  margin: 15px 0 0;
-  color: #b3b3b3;
-  font-size: 12px;
-}
-.form .message a {
-  color: #4CAF50;
-  text-decoration: none;
-}
-.form .register-form {
-  display: none;
-}
-.container {
-  position: relative;
-  z-index: 1;
-  max-width: 300px;
-  margin: 0 auto;
-}
-.container:before, .container:after {
-  content: "";
-  display: block;
-  clear: both;
-}
-.container .info {
-  margin: 50px auto;
-  text-align: center;
-}
-.container .info h1 {
-  margin: 0 0 15px;
-  padding: 0;
-  font-size: 36px;
-  font-weight: 300;
-  color: #1a1a1a;
-}
-.container .info span {
-  color: #4d4d4d;
-  font-size: 12px;
-}
-.container .info span a {
-  color: #000000;
-  text-decoration: none;
-}
-.container .info span .fa {
-  color: #EF3B3A;
-}
-body {
-  background: #76b852; /* fallback for old browsers */
-  background: rgb(141,194,111);
-  background: linear-gradient(90deg, rgb(22, 135, 237), rgb(20, 55, 90));
-  font-family: "Roboto", sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;     
-  height: 10rem;
-  text-align: center; 
-}
-        </style>
-    </head>
-    <body class="back">
-        <a href="index.php" class="topics">スポーツtopics</a>
-        <div class="login-page">
-            <div class="form">
-                <form action="Login.php" method="post">
-                        <label for="user_name">ユーザー名</label><br>
-                        
-                        <input id="user_id" name="user_name" type="text" /><br>
-                        <?php if (isset($err['user_name'])) : ?>
-                            <?php endif; ?>
+    <div class="button_wrapper2">
+        <a href="Post.php" class="btn02 pushright"><span>記事の投稿</span></a>
+    </div>
 
-                        <label for="">パスワード</label><br>
-                
-                        <input id="password" name="password" type="password" />
-                        <?php if (isset($err['password'])) : ?>
-                        <?php endif; ?>
-            
-                    <div class="loginbutton">
-                        <button type="submit" class="roguinn">ログイン</button>
-                    </div>
-                    <a href="Register.php">新規登録はこちら</a>
-                </form>
-                        </div>
-                        </div>
-    </body>
+    <div class="button_wrapper2">
+        <a href="index.php" class="btn02 pushright"><span>ログアウト</span></a>
+    </div>
+
+    </div>
+    </div>
+</body>
 </html>
